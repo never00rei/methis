f821f2e68d17b7fb4eb36301e4d5c5205787a65f
James Washington <jwashington104.jw@gmail.com>
2019-06-22 20:55:30 +0100

    Push to develop
    Push initial setup to VC

---
+++ config.py
@@ -0,0 +1,9 @@
+import yaml
+from os import getenv as getenv
+
+HTTP_PORT = getenv('HTTP_PORT', 5000)
+HTTP_ADDR = getenv('HTTP_ADDR', '0.0.0.0')
+DEBUG = getenv('DEBUG', True)
+THREADS_PER_PAGE = getenv('THREADS_PER_PAGE', 4)
+CSRF_ENABLED = getenv('CSRF_ENABLED', True)
+CSRF_SESSION_KEY = getenv('CSRF_SESSION_KEY','SECRET')
